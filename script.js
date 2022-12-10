df911a50f142ccaa074b16448dd83058230f4177
carsonadair <carsonadairphoto@gmail.com>
2022-12-09 19:10:56 -0500

    cleaned up code and added semicolons

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function askLength() {
     askLength();
   } else {
     return charLength;
-  }
+  };
 };
 
 function askUpletters() {
@@ -27,39 +27,39 @@ function askUpletters() {
     return upLetters.split("");
   } else {
     return "";
-  }
-}
+  };
+};
 
 function askLowletters() {
   var askLowLettersv = confirm("Do you want lowercase letters in your password?");
   if (askLowLettersv) {
     return lowLetters.split("");
-  }else {
+  } else {
     return "";
-  }
-}
+  };
+};
 
 function askNum() {
   var askNumv = confirm("Do you want numbers in your password?");
   if (askNumv) {
     return numbers.split("");
-  }else {
+  } else {
     return "";
-  }
-}
+  };
+};
 
 function askSym() {
   var askSymv = confirm("Do you want special symbols in your password?");
   if (askSymv) {
     return symbols.split("");
-  }else {
+  } else {
     return "";
-  }
-}
+  };
+};
 
 function generatePassword() {
   var characters = [];
-  var password = [];
+  var pass = [];
   console.log("Hey! You clicked the button!");
   var pwLength = askLength();
   characters.push(...askUpletters());
@@ -68,62 +68,18 @@ function generatePassword() {
   characters.push(...askSym());
   console.log(characters);
   if (characters.length === 0) {
-    alert("You must choose at least one type of character.");
+    alert("You must choose at least one type of character.")
     generatePassword();
-  }
-  for (i=0; i<pwLength; i++) {
+  }; 
+  for (i = 0; i < pwLength; i++) {
     var random = Math.floor(Math.random() * (characters.length - 1));
-    password.push(characters[random]);
+    pass.push(characters[random]);
     console.log(random);
-  }
+  };
 
-  return password.join("");
- /* var charLength = prompt("How many characters should your password be?");
-  if (charLength < 8) {
-    alert("Password too small, please pick a number between 8 and 128.")
-    askLength();
-  } else if (charLength > 128) {
-    alert("Password too long, please pick a number between 8 and 128.")
-    askLength();
-  } else if (isNaN(charLength)) {
-    alert("Not valid. Please select a number between 8 and 128.")
-    askLength();
-  } else {
-    return charLength;
-  }
+  return pass.join("");
 
-  var askUpLettersv = confirm("Do you want uppercase letters in your password?");
-  if (askUpLettersv) {
-    return upLetters.string;
-  }
-
-  var askLowLettersv = confirm("Do you want lowercase letters in your password?");
-  if (askLowLettersv) {
-    return lowLetters.string;
-  }
-
-  var askNumv = confirm("Do you want numbers in your password?");
-  if (askNumv) {
-    return numbers.string;
-  }
-
-  var askSymv = confirm("Do you want special symbols in your password?");
-  if (askSymv) {
-    return symbols.string;
-  } */
-
-
-
-  /* 
-  The alert should ask how many characters the password should have - minimum 8, maximum 128
-  The alert should then ask if it should use special characters.
-  The alert should then ask if it should include numbers.
-  The alert should then ask if it should include upper case letters.
-  The alert should then ask if it should use lowercase letters.
-  
-
-  */
-}
+};
 
 // Write password to the #password input
 function writePassword() {
